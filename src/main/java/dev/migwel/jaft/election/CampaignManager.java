3ef2db8f740a920bfc5c87d431a8fe9822af004d
migwel <migonzalez1991@gmail.com>
2022-12-17 21:44:19 +0100

    Remove unused imports

--- src/main/java/dev/migwel/jaft/election/CampaignManager.java
+++ src/main/java/dev/migwel/jaft/election/CampaignManager.java
@@ -1,7 +1,5 @@
 package dev.migwel.jaft.election;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
